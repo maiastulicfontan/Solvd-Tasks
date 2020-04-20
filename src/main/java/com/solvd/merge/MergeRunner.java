dbf879342e800a03efea75569e3a9b664b096e24
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2020-04-20 18:52:46 -0300

    Changed message printed in MergeRunner.java

--- src/main/java/com/solvd/merge/MergeRunner.java
+++ src/main/java/com/solvd/merge/MergeRunner.java
@@ -3,7 +3,7 @@ package com.solvd.merge;
 public class MergeRunner {
 
 	public static void main(String[] args) {
-		System.out.println("This is a random message");
+		System.out.println("I've changed my random message");
 
 	}
 
