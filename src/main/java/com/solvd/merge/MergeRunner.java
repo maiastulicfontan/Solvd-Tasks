ff53411f33cf7426379521e7a30e0b49f7e67019
Estanislao Perez Pena <35465776+EstaniPP@users.noreply.github.com>
2020-04-20 19:59:28 -0300

    Update MergeRunner.java

--- src/main/java/com/solvd/merge/MergeRunner.java
+++ src/main/java/com/solvd/merge/MergeRunner.java
@@ -6,6 +6,7 @@ public class MergeRunner {
 
 		System.out.println("I've changed my random message for the third time");
 		System.out.println("Here's another random message");
+		System.out.println("This is Estani's message");
 	}
 
 }
