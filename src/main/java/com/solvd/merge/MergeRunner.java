54d6b9a84a6d7b60b4b7710bcdd338a84b626f62
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2020-04-20 20:33:35 -0300

    Fixed conflict in MergeRunner.java

--- src/main/java/com/solvd/merge/MergeRunner.java
+++ src/main/java/com/solvd/merge/MergeRunner.java
@@ -7,6 +7,7 @@ public class MergeRunner {
 		System.out.println("I've changed my random message for the third time");
 		System.out.println("Facu was here");
 		System.out.println("Here's another random message");
+		System.out.println("Here's an exception");
 		System.out.println("This is Estani's message");
 	}
 
