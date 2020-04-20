9e992a3b03593a323c86a081c35593c7c8152cac
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2020-04-20 18:56:32 -0300

    Done some changes

--- src/main/java/com/solvd/merge/MergeRunner.java
+++ src/main/java/com/solvd/merge/MergeRunner.java
@@ -3,7 +3,7 @@ package com.solvd.merge;
 public class MergeRunner {
 
 	public static void main(String[] args) {
-		System.out.println("I've changed my random message");
+		System.out.println("I've changed my random message again");
 
 	}
 
