544b96616848fa894a71353f53a34a39ca40614a
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2020-04-20 16:36:34 -0300

    Changed a method in files/MyFileUtils.java

--- src/main/java/com/solvd/files/FileRunner.java
+++ src/main/java/com/solvd/files/FileRunner.java
@@ -13,13 +13,8 @@ public class FileRunner {
 		
 		//
 		try {
-			File textFileA = new File("src/main/resources/textfileA.txt");
-			/*File textFileB = new File("src/main/resources/textfileB.txt");
-			File textFileC = new File ("src/main/resources/textfileC.txt");
-			MyFileUtils.countWordOccurrences(textFileA);
-			MyFileUtils.countWordsNoOverlap(textFileB);
-			MyFileUtils.countWordsSimpleWay(textFileC);*/
-			MyFileUtils.countWordsHashMap(textFileA);
+			File textFile = new File("src/main/resources/textfile.txt");
+			MyFileUtils.countWords(textFile);
 			
 		} catch (IOException e) {
 			LOGGER.error("Can't find some of the files",e);
--- src/main/java/com/solvd/files/MyFileUtils.java
+++ src/main/java/com/solvd/files/MyFileUtils.java
@@ -2,10 +2,7 @@ package com.solvd.files;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -29,49 +26,9 @@ public class MyFileUtils {
 		return tmpArray;
 	}
 	
-	/*this method allows string overlap. for example, if "the" counts as a word and there are other words containing "the", such as "there" or "they", this method
-	will count those strings as matches for the string "the" */
-	public static void countWordOccurrences(File textFile) throws IOException{
-		LOGGER.info("Counting word occurrences in file allowing string overlap: "+textFile.getName());
-		String string = FileUtils.readFileToString(textFile, "UTF-8");
-		String[] list = MyFileUtils.cleanStringToArray(string);
-		Set <String> set = new HashSet <String>(Arrays.asList(list));
-		FileUtils.writeStringToFile(textFile, "\nOCCURRENCES OF WORDS IN THE FILE\n","UTF-8", true);
-		for (String word : set) {
-			FileUtils.writeStringToFile(textFile,"\n"+ word+": "+StringUtils.countMatches(string, word), "UTF-8", true);
-		}
-	}
-	
-	//this method doesn't allow string overlap
-	public static void countWordsNoOverlap(File textFile) throws IOException {
-		LOGGER.info("Counting word occurrences in file without allowing string overlap: "+textFile.getName());
-		String string = FileUtils.readFileToString(textFile, "UTF-8");
-		String[] list = MyFileUtils.cleanStringToArray(string);
-		Set <String> set = new HashSet <String>(Arrays.asList(list));
-		FileUtils.writeStringToFile(textFile, "\n\nOCCURRENCES OF WORDS IN THE FILE\n","UTF-8", true);
-		for (String notDuplicatedWord : set) {
-			int count = 0;
-			for (String word : list ) {
-				if (StringUtils.equals(notDuplicatedWord, word)) { 
-					count++;
-				}
-			}
-			FileUtils.writeStringToFile(textFile, "\n"+ notDuplicatedWord+ ":" + count, "UTF-8", true);
-		}
-	}
-	
-	/*this method, while a little bit more simple, will count as separate words those which are followed by an empty space and those followed by a punctuation mark.
-	for example, according to this, "you" and "you." are two different words. it also allows string overlap */
-	public static void countWordsSimpleWay (File textFile) throws IOException{
-		String string = FileUtils.readFileToString(textFile, "UTF-8");
-		String[] list = MyFileUtils.cleanStringToArray(string);
-		Set <String> set = new HashSet <String>(Arrays.asList(list));
-		for (String word : set) {
-			FileUtils.writeStringToFile(textFile,"\n"+ word+": "+StringUtils.countMatches(string, word), "UTF-8", true);
-		}
-	}
-	
-	public static void countWordsHashMap (File textFile) throws IOException {
+
+	public static void countWords (File textFile) throws IOException {
+		LOGGER.info("Counting words in text file...");
 		String string = FileUtils.readFileToString(textFile, "UTF-8");
 		HashMap <String, Integer> tmpMap = new HashMap <String, Integer>();
 		String[] list = MyFileUtils.cleanStringToArray(string);
