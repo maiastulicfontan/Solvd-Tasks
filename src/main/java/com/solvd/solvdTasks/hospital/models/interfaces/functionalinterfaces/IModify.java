cbddacc3e963388d2021b81a37b47d74df6bb498
Maia Stulic Fontan <maiastulicfontan@gmail.com>
2020-05-08 04:04:03 -0300

    Removed some files

--- src/main/java/com/solvd/solvdTasks/hospital/models/interfaces/functionalinterfaces/IModify.java
+++
@@ -1,6 +0,0 @@
-package com.solvd.solvdTasks.hospital.models.interfaces.functionalinterfaces;
-
-@FunctionalInterface
-public interface IModify<T,U> {
-	void modify(T t, U u);
-}
--- src/main/java/com/solvd/solvdTasks/hospital/models/interfaces/functionalinterfaces/IPrint.java
+++
@@ -1,5 +0,0 @@
-package com.solvd.solvdTasks.hospital.models.interfaces.functionalinterfaces;
-
-public interface IPrint<T> {
-	void print(T type);
-}
